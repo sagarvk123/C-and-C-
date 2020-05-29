8afbd8639f7e7c8d64843ffe7ace11db5dfd4e0b
sagarvk123 <66108594+sagarvk123@users.noreply.github.com>
2020-05-29 12:46:39 +0530

    Add files via upload

---
+++ multiple user.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,  num2, num3, sum;
+	printf("enter the three numbers, num1, num2, num3:");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	sum=num1+num2+num3;
+	printf("Sum of num1,num2,num3 %d",sum);
+	return 0;
+}
